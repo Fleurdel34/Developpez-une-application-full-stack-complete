2372cd8efeca5c2f8903a6a2da172cf6d08a850c
Fleurdel34 <betdevelweb0953@gmail.com>
2026-03-06 07:41:18 +0100

    modified configuration security et update configuration tests

--- back/src/main/java/com/openclassrooms/mddapi/security/JwtFilter.java
+++ back/src/main/java/com/openclassrooms/mddapi/security/JwtFilter.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,7 +21,7 @@ import java.io.IOException;
  * @params request, response, filterChain
  */
 
-@Service
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final UserServiceImpl userService;
